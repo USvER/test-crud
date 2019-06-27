fef1cd5ed04be6f9842563e838111a2df443e289
Yuriy Brazhnyk <brazhnyk_yuriy@ukr.net>
2019-06-27 17:25:25 +0300

    Added simple tests for the Posts API

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts API', type: :request do
+  let!(:posts) { create_list(:post, 10) }
+  let(:post_id) { posts.first.id }
+
+  describe 'GET /posts' do
+    before { get '/posts' }
+    
+    it 'returns 10 nonempty posts' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /posts/:id' do
+    before { get "/posts/#{post_id}" }
+
+    context 'when the record exists' do
+      it 'returns the post' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(post_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:post_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'POST /posts' do
+    # valid payload
+    let(:valid_attributes) { { title: 'TEST TITLE', body: 'TEST BODY' } }
+
+    context 'when the request is valid' do
+      before { post '/posts', params: valid_attributes }
+
+      it 'creates a post' do
+        expect(json['title']).to eq('TEST TITLE')
+        expect(json['body']).to eq('TEST BODY')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/posts', params: { title: 'NO BODY PROVIDED' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(json['errors']).not_to be_empty
+      end
+    end
+  end
+
+  describe 'PUT /posts/:id' do
+    let(:valid_attributes) { { title: 'NEW TITLE' } }
+
+    context 'when the record exists' do
+      before { put "/posts/#{post_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /posts/:id' do
+    before { delete "/posts/#{post_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
