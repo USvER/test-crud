a942c6e3fad012f0aa0ce276c4b1fd58ab56ab1a
Yuriy Brazhnyk <brazhnyk_yuriy@ukr.net>
2019-07-03 19:03:27 +0300

    Added simple test for form errors

--- spec/views/admin/posts/edit.haml_spec.rb
+++ spec/views/admin/posts/edit.haml_spec.rb
@@ -9,4 +9,14 @@ describe "admin/posts/edit.haml" do
     
     expect(rendered).to have_tag("form", with: {action: admin_post_path(new_post)})
   end
+
+  it "display error messages on bad input" do
+    new_post = Post.new({title: "", body: ""})
+    new_post.save
+    assign(:post, new_post)
+    
+    render
+
+    expect(rendered).to have_tag("div", with: {class: "alert-danger"})
+  end
 end
